3c84415d792c2585aaca7cac0018a1ef469e82df
Mahadi001 <sadat.himel@gmail.com>
2021-10-09 18:59:05 +0600

    update

--- page.php
+++ page.php
@@ -4,7 +4,8 @@ the_post();
 the_content( );
 
 echo "</hr>";
-echo get_transient( "hello world" );
+echo get_transient( "res_count" );
+// echo get_transient( "hello world" );
 echo "</hr>";
 
 get_footer( );
